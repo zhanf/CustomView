90cd90d587321e7a2f33d7d18cc48dc6add76e36
zf <791588195@qq.com>
2017-08-22 10:02:20 +0800

    fixed conflict

--- ZfCustomView/app/src/main/java/zhanf/com/zfcustomview/widget/SelectorTextview.java
+++ ZfCustomView/app/src/main/java/zhanf/com/zfcustomview/widget/SelectorTextview.java
@@ -1,13 +1,15 @@
 package zhanf.com.zfcustomview.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
+import zhanf.com.zfcustomview.R;
 
 /**
  * Created by Administrator on 2017/8/18.
@@ -20,6 +22,10 @@ public class SelectorTextview extends AppCompatTextView {
     private int width;
 
     private boolean isSelected;
+    private RectF rectF;
+    private int selectorColor;
+    private int normalColor;
+    private int radius;
 
 
     public SelectorTextview(Context context) {
@@ -33,18 +39,36 @@ public class SelectorTextview extends AppCompatTextView {
     public SelectorTextview(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+
+        initStyleAttr(attrs);
+
         paint = new Paint();
 
     }
 
+    private void initStyleAttr(AttributeSet attrs) {
+
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.style_selector_tv);
+
+        normalColor = a.getColor(R.styleable.style_selector_tv_normal, 0XFFFFFFFF);
+        selectorColor = a.getColor(R.styleable.style_selector_tv_selector, 0XFFFFFFFF);
+        radius = a.getColor(R.styleable.style_selector_tv_radius, 0);
+
+        if (0XFFFFFFFF != selectorColor) {
+            isSelected = true;
+        }
+
+        a.recycle();
+    }
+
+
     @Override
     protected void onDraw(Canvas canvas) {
-        RectF rectF = new RectF(0, 0, width, height);
         if (isSelected) {
-            paint.setColor(Color.BLUE);
+            paint.setColor(selectorColor);
             canvas.drawRoundRect(rectF, 30, 30, paint);
         } else {
-            paint.setColor(Color.BLACK);
+            paint.setColor(normalColor);
             canvas.drawRoundRect(rectF, 30, 30, paint);
         }
         super.onDraw(canvas);
@@ -55,6 +79,8 @@ public class SelectorTextview extends AppCompatTextView {
         super.onWindowFocusChanged(hasWindowFocus);
         height = getHeight();
         width = getWidth();
+        rectF = new RectF(0, 0, width, height);
+
     }
 
     @Override
