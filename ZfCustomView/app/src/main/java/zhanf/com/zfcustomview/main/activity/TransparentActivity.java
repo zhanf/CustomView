92bc184f43ad4b32150417bda2f132179cb0fd62
zf <791588195@qq.com>
2017-09-21 23:49:35 +0800

    add ->switch 设置样式

--- ZfCustomView/app/src/main/java/zhanf/com/zfcustomview/main/activity/TransparentActivity.java
+++ ZfCustomView/app/src/main/java/zhanf/com/zfcustomview/main/activity/TransparentActivity.java
@@ -1,15 +1,44 @@
 package zhanf.com.zfcustomview.main.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import zhanf.com.zfcustomview.R;
 
 public class TransparentActivity extends AppCompatActivity {
+
+    @BindView(R.id.sw_switch)
+    Switch swSwitch;
+    @BindView(R.id.cb_check_box)
+    CheckBox cbCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transparent);
+        ButterKnife.bind(this);
+
+        swSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                } else {
+
+                }
+            }
+        });
+
+        cbCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
     }
 }
